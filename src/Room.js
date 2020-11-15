49a8c86dadd8d86498e0edcda7c0cafc34cd18a9
Muhammad Osama Shahzad <usamashahzad19@gmail.com>
2020-11-15 23:08:59 +0500

    Completed Assignment by adding 4 more buttons of +, -, ON and OFF

--- src/Room.js
+++ src/Room.js
@@ -9,6 +9,7 @@ import './Room.css'
 function Room() {
   const [isLit, setLit] = useState(false);
   let [age, setAge] = useState(23)
+  let [temp, setTemp] = useState(73)
     
 
 //BACKTICK is enclosing `room` in classname
@@ -18,15 +19,36 @@ function Room() {
         <br/>
         Age: {age}
         <br/>
-        <button onClick = {() =>setLit(!isLit) } >
+        Temperature: {temp}
+        <br/>
+        <button style = {{color : 'red'}} onClick = {() =>setLit(!isLit) } >
           Toggle Light</button>
         <br/>
         
-        <button  onClick = {() =>{
+        <button style = {{color : 'green'}} onClick = {() =>setLit(true) } >
+          ON Light</button>
+        <br/>
+
+        <button style = {{color : 'black'}} onClick = {() =>setLit(false) } >
+          OFF Light</button>
+        <br/>
+
+        <button style= {{color : 'blue'}}  onClick = {() =>{
                                   console.log('Increase Age Button Clicked')
                                   setAge(++age)
                                 }}>
           Add Age</button>
+        <br/>
+        <button onClick = {() =>{
+                                  console.log('Increase Temp Button Clicked')
+                                  setTemp(++temp)
+                                }}>
+          +</button>
+          <button onClick = {() =>{
+                                  console.log('Increase Temp Button Clicked')
+                                  setTemp(--temp)
+                                }}>
+          -</button>
     </div>
   );
 }
